437011e336f065ab32c2b6ebda7a37dba5401591
Lukas Plenk <lukasplenk@yahoo.de>
2025-03-10 22:20:13 +0100

    Setup Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:story_cube_app/app.dart';
 
 import 'bloc/story_cube_cubit.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     BlocProvider(
       create: (context) => StoryCubeCubit()..loadAppData(),
