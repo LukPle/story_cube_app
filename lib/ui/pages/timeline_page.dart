961bb596d441ea7591622e4fac7f18e007b3daa0
Lukas Plenk <lukasplenk@yahoo.de>
2025-03-12 14:52:15 +0100

    Added bloc builder to timeline page

--- lib/ui/pages/timeline_page.dart
+++ lib/ui/pages/timeline_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
-import 'package:story_cube_app/models/example_data.dart';
 import 'package:story_cube_app/models/memory_model.dart';
 import 'package:story_cube_app/ui/widgets/app_scaffold.dart';
 import 'package:story_cube_app/ui/widgets/empty_state_placeholder.dart';
 
+import '../../bloc/bloc.dart';
 import '../../constants/sizes.dart';
 import '../widgets/timeline_entry.dart';
 
@@ -13,34 +14,42 @@ class TimelinePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<MemoryModel> memories = memoryData;
+    return BlocBuilder<StoryCubeCubit, StoryCubeState>(
+      bloc: BlocProvider.of<StoryCubeCubit>(context),
+      builder: (context, state) {
+        if (state is StoryCubeLoadSuccess) {
+          final List<MemoryModel> memories = state.memories;
+          memories.sort((a, b) => b.dateTime.compareTo(a.dateTime));
 
-    memories.sort((a, b) => b.dateTime.compareTo(a.dateTime));
+          Map<String, List<MemoryModel>> groupedMemories = {};
+          for (var memory in memories) {
+            String monthYear = DateFormat('MMMM yyyy').format(memory.dateTime);
+            groupedMemories.putIfAbsent(monthYear, () => []).add(memory);
+          }
 
-    Map<String, List<MemoryModel>> groupedMemories = {};
-    for (var memory in memories) {
-      String monthYear = DateFormat('MMMM yyyy').format(memory.dateTime);
-      groupedMemories.putIfAbsent(monthYear, () => []).add(memory);
-    }
+          return AppScaffold(
+            pageTitle: 'Timeline',
+            children: [
+              const SizedBox(height: AppSizes.size_16),
+              Column(
+                children: groupedMemories.isNotEmpty
+                    ? List.generate(groupedMemories.length, (index) {
+                        final entry = groupedMemories.entries.elementAt(index);
+                        return TimelineEntry(
+                          monthYear: entry.key,
+                          memories: entry.value,
+                          isLastEntry: index == groupedMemories.length - 1,
+                        );
+                      })
+                    : [const EmptyStatePlaceholder(message: 'Recorded memories will appear here')],
+              ),
+              const SizedBox(height: AppSizes.size_64),
+            ],
+          );
+        }
 
-    return AppScaffold(
-      pageTitle: 'Timeline',
-      children: [
-        const SizedBox(height: AppSizes.size_16),
-        Column(
-          children: groupedMemories.isNotEmpty
-              ? List.generate(groupedMemories.length, (index) {
-                  final entry = groupedMemories.entries.elementAt(index);
-                  return TimelineEntry(
-                    monthYear: entry.key,
-                    memories: entry.value,
-                    isLastEntry: index == groupedMemories.length - 1,
-                  );
-                })
-              : [const EmptyStatePlaceholder(message: 'Recorded memories will appear here')],
-        ),
-        const SizedBox(height: AppSizes.size_64),
-      ],
+        return const Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
